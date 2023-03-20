2f66634fdec8f7ab49a905502c5eb27d29d3f158
zaman <zamanmehrab.12345@gmail.com>
2023-03-20 16:59:00 -0400

    Mehrab Data Confimation page

---
+++ app/src/main/java/project/stn991614740/grocerymanagerapp/DataConfirmationPage.kt
@@ -0,0 +1,11 @@
+package project.stn991614740.grocerymanagerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataConfirmationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_confirmation_page)
+    }
+}
