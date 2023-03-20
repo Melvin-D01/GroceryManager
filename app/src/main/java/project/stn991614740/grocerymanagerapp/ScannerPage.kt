adcd93548266ecd48afccdc91e13f647d1682403
zaman <zamanmehrab.12345@gmail.com>
2023-03-20 16:59:00 -0400

    Mehrab Data Confimation page

---
+++ app/src/main/java/project/stn991614740/grocerymanagerapp/ScannerPage.kt
@@ -0,0 +1,11 @@
+package project.stn991614740.grocerymanagerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScannerPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanner_page2)
+    }
+}
