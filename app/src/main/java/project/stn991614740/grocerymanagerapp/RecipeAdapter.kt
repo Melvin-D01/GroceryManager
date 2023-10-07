882dd9ba9532f88160d6e76151f99591bb83696c
Chi <daniel.le17@hotmail.com>
2023-10-07 12:55:41 -0400

    added comments to RecipeAdapter.kt and RecipeFragment.kt

--- app/src/main/java/project/stn991614740/grocerymanagerapp/RecipeAdapter.kt
+++ app/src/main/java/project/stn991614740/grocerymanagerapp/RecipeAdapter.kt
@@ -7,45 +7,55 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import project.stn991614740.grocerymanagerapp.databinding.RecipeItemBinding
 
+// Adapter class for displaying a list of recipes in a RecyclerView
 class RecipeAdapter(private val recipes: List<Recipe>) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
 
+    // Callback to be invoked when a recipe is clicked
     var onRecipeClicked: ((Recipe) -> Unit)? = null
 
-    // ViewHolder class to hold and bind the views using View Binding
+    // ViewHolder class that represents each item in the list
     inner class RecipeViewHolder(private val binding: RecipeItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
+        // Initialize listener to handle item click
         init {
             itemView.setOnClickListener {
                 val position = adapterPosition
+                // Check if the clicked position is valid
                 if (position != RecyclerView.NO_POSITION) {
+                    // Trigger the callback with the clicked recipe
                     onRecipeClicked?.invoke(recipes[position])
                 }
             }
         }
 
-        // Bind the data to your views
+        // Bind the data of a single recipe to the UI elements
         fun bind(recipe: Recipe) {
             // Convert the creationDate (Timestamp) to a readable date format
             val formattedDate = recipe.creationDate?.toDate()?.toString() ?: "Unknown Date"
             binding.creationDateTextView.text = formattedDate
+
+            // Display only the first three lines of the recipe
             val lines = recipe.recipe.split("\n")
             val firstTwoLines = lines.take(3).joinToString("\n").trim()
             binding.recipeTextView.text = firstTwoLines
         }
     }
 
-    // Inflate the item layout using View Binding and return the ViewHolder
+    // Called when the RecyclerView needs a new ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        // Inflate the layout for each list item using View Binding
         val binding = RecipeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return RecipeViewHolder(binding)
     }
 
-    // Bind the data to the ViewHolder
+    // Called by RecyclerView to bind the data at the specified position
     override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        // Log the data binding for debugging purposes
         Log.d(TAG, "Binding data for position $position: ${recipes[position]}")
+        // Bind the recipe data to the ViewHolder's UI elements
         holder.bind(recipes[position])
     }
 
-    // Return the total count of items
+    // Returns the total number of recipes in the list
     override fun getItemCount() = recipes.size
 }
