3a09e100ba4f6b8efd82d21d2bc7a7098f5266fb
Peter <wyroba.p@gmail.com>
2023-10-30 16:50:25 -0400

    Added the last settings options

--- app/src/main/java/project/stn991614740/grocerymanagerapp/SettingsFragment.kt
+++ app/src/main/java/project/stn991614740/grocerymanagerapp/SettingsFragment.kt
@@ -7,13 +7,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import project.stn991614740.grocerymanagerapp.databinding.FragmentSettingsBinding
 import com.google.firebase.firestore.FirebaseFirestore
@@ -35,6 +38,8 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setAppVersion()
 
         // Load the settings
         val sharedPreferences = requireContext().getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
@@ -61,6 +66,8 @@ class SettingsFragment : Fragment() {
                 // The user switched the theme to light mode
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
+
+
         }
 
         // Expiry notification switch initialization
@@ -101,6 +108,11 @@ class SettingsFragment : Fragment() {
             findNavController().navigate(action)
         }
 
+        binding.reportBugButton?.setOnClickListener {
+            showBugReportDialog()
+        }
+
+
         binding.deleteFridgeBtn?.setOnClickListener {
             // Show confirmation dialog
             val builder = AlertDialog.Builder(requireContext())
@@ -117,6 +129,7 @@ class SettingsFragment : Fragment() {
                     },
                     onFailure = { exception ->
                         Toast.makeText(requireContext(), "Error deleting Fridge: ${exception.message}", Toast.LENGTH_SHORT).show()
+                        Log.e("SettingsFragment", "Error deleting Fridge: ${exception.message}")
                     }
                 )
             }
@@ -125,6 +138,36 @@ class SettingsFragment : Fragment() {
             }
             builder.show()
         }
+
+
+
+        binding.deleteAccountButton?.setOnClickListener {
+            val user = FirebaseAuth.getInstance().currentUser
+            val emailProvider = "password"
+            val hasEmailAndPassword = user?.providerData?.any { it.providerId == emailProvider } == true
+
+            if (hasEmailAndPassword) {
+                // Prompt the user to re-enter their password
+                val passwordInputDialog = LayoutInflater.from(requireContext()).inflate(R.layout.password_input_dialog, null)
+                val passwordEditText = passwordInputDialog.findViewById<EditText>(R.id.passwordEditText)
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Re-authentication required")
+                    .setMessage("Please enter your password to proceed:")
+                    .setView(passwordInputDialog)
+                    .setPositiveButton("Submit") { _, _ ->
+                        val password = passwordEditText.text.toString().trim()
+                        reauthenticateAndDeleteUser(password)
+                    }
+                    .setNegativeButton("Cancel") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            } else {
+                // For users who signed in using social media or other providers
+                deleteUserAccount()
+            }
+        }
+
 
         binding.recipeDeleteBtn?.setOnClickListener {
             // Show confirmation dialog
@@ -172,6 +215,74 @@ class SettingsFragment : Fragment() {
 
     }
 
+    private fun reauthenticateAndDeleteUser(password: String) {
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null && user.email != null) {
+            val credential = EmailAuthProvider.getCredential(user.email!!, password)
+            user.reauthenticate(credential)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // User re-authenticated, proceed with account deletion
+                        deleteUserAccount()
+                    } else {
+                        // Re-authentication failed
+                        Toast.makeText(requireContext(), "Authentication failed. Please try again.", Toast.LENGTH_SHORT).show()
+                        Log.e("SettingsFragment", "Re-authentication failed: ${task.exception?.message}")
+                    }
+                }
+        } else {
+            Toast.makeText(requireContext(), "Failed to get user information for re-authentication.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun setAppVersion() {
+            val packageInfo = requireContext().packageManager.getPackageInfo(requireContext().packageName, 0)
+            val appVersion = "GroceryManager\nVersion ${packageInfo.versionName}"
+            binding.textViewAppVersion?.text = appVersion
+    }
+
+    private fun showBugReportDialog() {
+        val bugReportDialogView = LayoutInflater.from(requireContext()).inflate(R.layout.bug_report_dialog, null)
+        val bugReportEditText = bugReportDialogView.findViewById<EditText>(R.id.bugReportEditText)
+
+        AlertDialog.Builder(requireContext())
+            .setTitle("Report a Bug")
+            .setView(bugReportDialogView)
+            .setPositiveButton("Submit") { _, _ ->
+                val bugReport = bugReportEditText.text.toString().trim()
+                if (bugReport.isNotEmpty()) {
+                    saveBugReportToDatabase(bugReport)
+                } else {
+                    Toast.makeText(requireContext(), "Bug report cannot be empty", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+
+    private fun saveBugReportToDatabase(bugReport: String) {
+        val db = FirebaseFirestore.getInstance()
+        val bugReportsRef = db.collection("bugReports")
+
+        val bugReportData = hashMapOf(
+            "report" to bugReport,
+            "timestamp" to System.currentTimeMillis(),
+            "userId" to FirebaseAuth.getInstance().currentUser?.uid
+        )
+
+        bugReportsRef.add(bugReportData)
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(), "Bug report submitted successfully", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(requireContext(), "Failed to submit bug report: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 
 
     private fun cancelAlarm(receiverClass: Class<*>, requestCode: Int) {
@@ -183,5 +294,54 @@ class SettingsFragment : Fragment() {
         )
         alarmManager?.cancel(pendingIntent)
     }
+
+
+    private fun deleteUserAccount() {
+        // Show confirmation dialog
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Delete Account")
+        builder.setMessage("Are you sure you want to delete your account and all associated data? This action cannot be undone.")
+        builder.setPositiveButton("Delete") { _, _ ->
+            val userId = FirebaseAuth.getInstance().currentUser?.uid ?: return@setPositiveButton
+            val db = FirebaseFirestore.getInstance()
+
+            // Delete user's food list
+            val foodListRef = db.collection("users").document(userId).collection("foodList")
+            foodListRef.get().addOnSuccessListener { documents ->
+                val batch = db.batch()
+                for (document in documents) {
+                    batch.delete(foodListRef.document(document.id))
+                }
+                batch.commit()
+            }
+
+            // Delete user's recipes
+            val recipesRef = db.collection("users").document(userId).collection("recipes")
+            recipesRef.get().addOnSuccessListener { documents ->
+                val batch = db.batch()
+                for (document in documents) {
+                    batch.delete(recipesRef.document(document.id))
+                }
+                batch.commit()
+            }
+
+            // Delete user's account
+            FirebaseAuth.getInstance().currentUser?.delete()?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(requireContext(), "Account deleted successfully", Toast.LENGTH_SHORT).show()
+                    // Navigate to the start screen or login screen after deletion
+                    val action = SettingsFragmentDirections.actionSettingsFragmentToStartFragment()
+                    findNavController().navigate(action)
+                } else {
+                    Toast.makeText(requireContext(), "Failed to delete account: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        builder.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+
 
 }
